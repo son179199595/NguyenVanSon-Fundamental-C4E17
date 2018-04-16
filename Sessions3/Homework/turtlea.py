5f9e9f575ddaa2d1cc028ce229078c91adc0bf63
son179199595 <7loveforu10@gmail.com>
2018-04-16 10:11:41 -0700

    initial commit

---
+++ Sessions3/Homework/turtlea.py
@@ -0,0 +1,18 @@
+from turtle import *
+for i in range(3):
+    color("blue")
+    forward(100)
+    left(120)
+for i in range(4):
+    color("red")
+    forward(100)
+    left(90)
+for i in range(5):
+    color("blue")
+    forward(100)
+    left(72)
+for i in range(6):
+    color("red")
+    forward(100)
+    left(60)
+mainloop()
