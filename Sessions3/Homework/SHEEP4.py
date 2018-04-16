c7aabd3388f1b99ae18a9cd6ea8b18a4ce31884f
son179199595 <7loveforu10@gmail.com>
2018-04-16 10:05:28 -0700

    initial commit

--- Sessions3/Homework/SHEEP4.py
+++ Sessions3/Homework/SHEEP4.py
@@ -1,4 +1,11 @@
 print("Hello,my name is Son and here is my sheep sizes in crease 50:")
-list1=[5, 7, 300, 90, 24, 50, 75]
-x=list(filter(lambda x: x>=50, list1))
-print(x)
+size=[5, 7, 300, 90, 24, 50, 75]
+print(size)
+print("Now biggest sheep has size 300 let's shear it")
+
+size[2]=8
+print("After shearing, here is my flock")
+print(size)
+print("One month has passed, now here is my flock")
+
+print([x+50 for x in size])
