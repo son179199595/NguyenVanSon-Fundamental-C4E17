516f35d882c81cb437f654385036398e359f1610
son179199595 <7loveforu10@gmail.com>
2018-04-16 09:44:41 -0700

    =initial commit

--- Sessions3/Homework/SHEEP3.py
+++ Sessions3/Homework/SHEEP3.py
@@ -1,4 +1,8 @@
-print("Hello,my name is Son and here is my sheep sizes in crease 50:")
-list1=[5, 7, 300, 90, 24, 50, 75]
-x=list(filter(lambda x: x>=50, list1))
-print(x)
+print("Hello,my name is Son and here is my sheep sizes:")
+size=[5, 7, 300, 90, 24, 50, 75]
+print(size)
+print("Now biggest sheep has size 300 let's shear it")
+
+size[2]=8
+print("After shearing, here is my flock")
+print(size)
---
+++ Sessions3/Homework/SHEEP4.py
@@ -0,0 +1,4 @@
+print("Hello,my name is Son and here is my sheep sizes in crease 50:")
+list1=[5, 7, 300, 90, 24, 50, 75]
+x=list(filter(lambda x: x>=50, list1))
+print(x)
